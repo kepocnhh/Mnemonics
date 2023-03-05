8f9320a7044bd0cfd6dd193b8829db4864b741c9
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-05 23:47:56 +0300

    progress bar animation?

--- app/src/main/kotlin/org/kepocnhh/mnemonics/MainScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/mnemonics/MainScreen.kt
@@ -1,23 +1,22 @@
 package org.kepocnhh.mnemonics
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,38 +25,16 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.cancellable
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.isActive
 import kotlinx.coroutines.isActive
-import kotlinx.coroutines.job
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.Random
 import kotlin.math.absoluteValue
-import kotlin.math.min
-import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
 
 private val random = Random()
 
@@ -114,7 +91,6 @@ internal fun MainScreen() {
             println("$TAG: start: ${startState.value}")
             var progress: Float? by rememberSaveable { mutableStateOf(null) }
             println("$TAG: progress: $progress")
-            val lifecycleOwner = LocalLifecycleOwner.current
             LaunchedEffect(value, index, isPaused) {
                 println("$TAG: launched effect...")
                 if (!isPaused) {
@@ -132,6 +108,7 @@ internal fun MainScreen() {
                                 val duration = (now - start) / max
                                 if (duration < 1.0) {
                                     progress = duration.toFloat()
+                                    delay(250.milliseconds) // todo animation
                                 } else {
                                     progress = null
                                     startState.value = null
@@ -160,12 +137,6 @@ internal fun MainScreen() {
                     }
                 }
             }
-//            val text = when (value) {
-//                null -> "..."
-//                -2 -> "  0"
-//                -1 -> " 00"
-//                else -> "%03d".format(value)
-//            }
             BasicText(
                 modifier = Modifier
                     .fillMaxWidth(),
