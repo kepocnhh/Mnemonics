5cff8875b941d5c39a3724552ffef5094376bb5e
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-31 14:45:11 +0300

    icons...

--- app/src/main/kotlin/org/kepocnhh/mnemonics/MainScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/mnemonics/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
@@ -30,10 +32,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -137,6 +141,12 @@ internal fun MainScreen() {
         val TAG = "[MainScreen|${hashCode()}]"
         println("$TAG:\n\tcompose...")
         var toSettings by remember { mutableStateOf(false) }
+        Image(
+            modifier = Modifier.size(48.dp).align(Alignment.TopEnd),
+            painter = painterResource(id = R.drawable.gear),
+            contentDescription = App.Theme.strings.settings,
+            colorFilter = ColorFilter.tint(App.Theme.colors.foreground)
+        )
         Column(
             modifier = Modifier
                 .fillMaxWidth()
