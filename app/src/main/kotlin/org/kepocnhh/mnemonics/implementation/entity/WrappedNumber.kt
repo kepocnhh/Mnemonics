c6fa02b2d530a98d66350e084be33246c9d37014
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-08 15:59:17 +0300

    positive

--- app/src/main/kotlin/org/kepocnhh/mnemonics/implementation/entity/WrappedNumber.kt
+++ app/src/main/kotlin/org/kepocnhh/mnemonics/implementation/entity/WrappedNumber.kt
@@ -10,46 +10,41 @@ private class WrappedNumberImpl(
 ) : WrappedNumber {
     /**
      * https://en.wikipedia.org/wiki/Geometric_series
-     * raw = 0   , fmt = "  0", n=3, m=0, Sn=1110
-     * raw = 10  , fmt = " 00", n=3, m=1, Sn=1110
-     * raw = 110 , fmt = "000", n=3, m=2, Sn=1110
-     * raw = 1109, fmt = "999", n=3, m=2, Sn=1110
-     * raw = -1110, fmt = "  0", n=3, m=0, Sn=1110
-     * raw = -1109, fmt = "  1", n=3, m=0, Sn=1110
-     * raw = -1101, fmt = "  9", n=3, m=0, Sn=1110
-     * raw = -1100, fmt = " 00", n=3, m=1, Sn=1110
-     * raw = -1099, fmt = " 01", n=3, m=1, Sn=1110
-     * raw = -1058, fmt = " 42", n=3, m=1, Sn=1110
-     * raw = -1001, fmt = " 99", n=3, m=1, Sn=1110
-     * raw = -1000, fmt = "000", n=3, m=2, Sn=1110
-     * raw = -999 , fmt = "001", n=3, m=2, Sn=1110
-     * raw = -942 , fmt = "058", n=3, m=2, Sn=1110
-     * raw = -901 , fmt = "099", n=3, m=2, Sn=1110
-     * raw = -900 , fmt = "100", n=3, m=2, Sn=1110
-     * raw = -872 , fmt = "128", n=3, m=2, Sn=1110
-     * raw = -744 , fmt = "256", n=3, m=2, Sn=1110
-     * raw = -488 , fmt = "512", n=3, m=2, Sn=1110
-     * raw = -99  , fmt = "901", n=3, m=2, Sn=1110
-     * raw = -1   , fmt = "999", n=3, m=2, Sn=1110
+     * length: 3, number: 0   , fmt: "  0", n=0, Sn=0
+     * length: 3, number: 10  , fmt: " 00", n=1, Sn=10
+     * length: 3, number: 110 , fmt: "000", n=2, Sn=110
+     * length: 3, number: 1109, fmt: "999", n=2, Sn=110
      */
     override fun toString(): String {
-        val i = getPowerOfGeometricSeries(sum = getSumOfGeometricSeries(n = rank) + raw).toInt()
-        val formatted = getSumOfGeometricSeries(n = rank, m = i) + raw
-//        val i = getPowerOfGeometricSeries(sum = raw.toDouble()).toInt()
-//        val formatted = raw - getSumOfGeometricSeries(n = rank, m = i)
-        return "%${rank}s".format("%0${i + 1}d".format(formatted.toInt()))
+        val n = getPowerOfGeometricSeries(sum = raw.toDouble()).toInt()
+        val formatted = raw - getSumOfGeometricSeries(n = n)
+        return "%${rank}s".format("%0${n + 1}d".format(formatted.toInt()))
     }
 }
 
 /**
  * @param a is coefficient
  * @param r is the common ratio
- * @return sum of a*r.pow(n) + a*r.pow(n - 1) + ... + a*r.pow(m - 1) + a*r.pow(m)
+ * @return sum of a*r.pow(n) + a*r.pow(n - 1) + ... + a*r.pow(2) + a*r
  */
 private fun getSumOfGeometricSeries(
     a: Int = 1,
     r: Double = 10.0,
-    m: Int = 0,
+    n: Int
+): Double {
+    require(n >= 0) { "n($n) is negative!" }
+    return r * a * (1.0 - r.pow(n)) / (1.0 - r)
+}
+
+/**
+ * @param a is coefficient
+ * @param r is the common ratio
+ * @return sum of a*r.pow(n) + a*r.pow(n - 1) + ... + a*r.pow(m + 1) + a*r.pow(m)
+ */
+private fun getSumOfGeometricSeries(
+    a: Int = 1,
+    r: Double = 10.0,
+    m: Int,
     n: Int,
 ): Double {
     require(m >= 0) { "m($m) is negative!" }
@@ -89,8 +84,8 @@ private fun getPowerOfGeometricSeries10(
 }
 
 private fun getRange(rank: Int): IntRange {
-    val start = -getSumOfGeometricSeries(n = rank)
-    return start.toInt() until 0
+    val end = getSumOfGeometricSeries(n = rank)
+    return 0 until end.toInt()
 }
 
 internal fun Int.wrapped(rank: Int): WrappedNumber {
--- app/src/test/kotlin/org/kepocnhh/mnemonics/implementation/entity/WrappedNumberTest.kt
+++ app/src/test/kotlin/org/kepocnhh/mnemonics/implementation/entity/WrappedNumberTest.kt
@@ -8,10 +8,10 @@ internal class WrappedNumberTest {
     fun toStringRank1Test() {
         val rank = 1
         mapOf(
-            -10 to "0",
-            -9  to "1",
-            -2  to "8",
-            -1  to "9",
+            0 to "0",
+            1 to "1",
+            8 to "8",
+            9 to "9",
         ).forEach { (raw, expected) ->
             val number = raw.wrapped(rank = rank)
             assertEquals(expected, number.toString())
@@ -22,13 +22,15 @@ internal class WrappedNumberTest {
     fun toStringRank2Test() {
         val rank = 2
         mapOf(
-            -110 to " 0",
-            -102 to " 8",
-            -92  to "08",
-            -84  to "16",
-            -68  to "32",
-            -58  to "42",
-            -1   to "99",
+            0   to " 0",
+            1   to " 1",
+            9   to " 9",
+            10  to "00",
+            18  to "08",
+            26  to "16",
+            42  to "32",
+            74  to "64",
+            109 to "99",
         ).forEach { (raw, expected) ->
             val number = raw.wrapped(rank = rank)
             assertEquals(expected, number.toString())
@@ -39,27 +41,18 @@ internal class WrappedNumberTest {
     fun toStringRank3Test() {
         val rank = 3
         mapOf(
-//            0    to "  0",
-//            10   to " 00",
-//            100  to "000",
-//            1099 to "999",
-            -1110 to "  0",
-            -1109 to "  1",
-            -1101 to "  9",
-            -1100 to " 00",
-            -1099 to " 01",
-            -1058 to " 42",
-            -1001 to " 99",
-            -1000 to "000",
-            -999  to "001",
-            -942  to "058",
-            -901  to "099",
-            -900  to "100",
-            -872  to "128",
-            -744  to "256",
-            -488  to "512",
-            -99   to "901",
-            -1    to "999",
+            0    to "  0",
+            1    to "  1",
+            9    to "  9",
+            10   to " 00",
+            11   to " 01",
+            52   to " 42",
+            109  to " 99",
+            110  to "000",
+            111  to "001",
+            238  to "128",
+            622  to "512",
+            1109 to "999",
         ).forEach { (raw, expected) ->
             val number = raw.wrapped(rank = rank)
             assertEquals(expected, number.toString())
@@ -70,8 +63,18 @@ internal class WrappedNumberTest {
     fun toStringRank9Test() {
         val rank = 9
         mapOf(
-            -1_111_111_110 to "        0",
-            -1             to "999999999",
+            0             to "        0",
+            1             to "        1",
+            10            to "       00",
+            52            to "       42",
+            109           to "       99",
+            622           to "      512",
+            2134          to "     1024",
+            3158          to "     2048",
+            5206          to "     4096",
+            9302          to "     8192",
+            27494         to "    16384",
+            1_111_111_109 to "999999999",
         ).forEach { (raw, expected) ->
             val number = raw.wrapped(rank = rank)
             assertEquals(expected, number.toString())
